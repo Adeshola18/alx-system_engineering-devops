bb5dfc30f7038f473002e9cc240b7b87e0d73d79
Timothy Atobatele <gbeminija.oladimeji.com>
2022-10-15 01:11:46 +0100

    correction

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,7 @@
-# Installs puppet-lint
+# Installs flask package.
+# ensure its version 2.1.0
 
-package { 'puppet-lint':
-  ensure   => '2.1.1',
-  provider => 'gem',
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
 }
